561bc69a6a97937810ed8eacfa4af7b30d520b93
Felipe Thomé <119542554+felipetn1989@users.noreply.github.com>
2023-01-09 07:55:22 -0300

    added forEach

--- scripts/script.js
+++ scripts/script.js
@@ -8,15 +8,15 @@ let images = document.querySelectorAll(".error_img");
 
 function verify(x) {
   x.preventDefault();
-  for (let i = 0; i < inputs.length; i++) {
-    if (inputs[i].value.length == 0) {
-      inputs[i].style.border = "1px solid var(--Red)";
-      spans[i].style.display = "block";
+  inputs.forEach((input, index) => {
+    if (input.value.length == 0) {
+      input.style.border = "1px solid var(--Red)";
+      spans[index].style.display = "block";
     } else {
-      inputs[i].style.border = "1px solid lightgray";
-      spans[i].style.display = "none";
+      input.style.border = "1px solid lightgray";
+      spans[index].style.display = "none";
     }
-  }
+  });
 
   let mailformat =
     /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/;
