ca31f919517a10fec960bed4c0a682ae374fe920
Felipe Thomé <119542554+felipetn1989@users.noreply.github.com>
2023-01-11 18:09:49 -0300

    added  comments

--- scripts/script.js
+++ scripts/script.js
@@ -1,10 +1,14 @@
+/* First, I added an event listener to the 'Claim your free trial' button. When I click on it, it will call the function verify */
+
 claim.addEventListener("click", verify);
+
+// Creating variables to be used in the function
 
 let inputs = document.querySelectorAll(".personal_info");
 
 let spans = document.querySelectorAll(".errormsg");
 
-let images = document.querySelectorAll(".error_img");
+/* First, the function prevents the browser default behavior (the browser comes with a script that shows 'this isn't a valid e-mail' or 'please fill out this form', but the challenge requires a custom script). Using a forEach command, the function will check each input to see if it has been completed or not, and display the error message and change the border accordinly */
 
 function verify(x) {
   x.preventDefault();
@@ -17,6 +21,8 @@ function verify(x) {
       spans[index].style.display = "none";
     }
   });
+
+/* The second part of the code uses a RegEx to check if the e-mail has a valid format. In this case, no need to use a forEach, since we only need to check the e-mail input. If it isn't valid, we do the required transformations and display error messages */
 
   let mailformat =
     /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/;
